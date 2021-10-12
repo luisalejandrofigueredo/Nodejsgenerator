c8736dbaac6026fcf7717e17877fbc8b430d9941
luisalejandrofigueredo <luisalejandrofigueredo@gmail.com>
2021-10-12 18:03:17 -0300

    12/10

--- src/assets/files/controller/index.controller.ts
+++ src/assets/files/controller/index.controller.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from 'express';
 class IndexController {
   public index = (req: Request, res: Response, next: NextFunction): void => {
     try {
-      res.sendStatus(200);
+      res.send('Created with Node Js Generator');
     } catch (error) {
       next(error);
     }
