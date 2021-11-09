2d15e776b7d13b38cfd128fac7e8876baab90133
luisalejandrofigueredo <luisalejandrofigueredo@gmail.com>
2021-11-09 11:03:43 -0300

    09/11

--- src/app/service/extension/extension.service.ts
+++ src/app/service/extension/extension.service.ts
@@ -3,6 +3,7 @@ import { ElectronService } from 'ngx-electron';
 import { ConfigService } from '../config.service';
 import { ControllersExtensionService } from "../controllers-extension.service";
 import { Extension } from "../../interfaces/extension";
+import { ServicesExtension } from "../../interfaces/services-extension";
 @Injectable({
   providedIn: 'root'
 })
@@ -26,7 +27,7 @@ export class ExtensionService {
     this.lineGenerating += "import { Router } from 'express';\n"
     const controllers = this.controllersService.getControllers(index, indexRoute);
     controllers.forEach(controller => {
-      this.lineGenerating += `import {${controller.name}} from '../controllers/${controller.name}.ts'\n`
+      this.lineGenerating += `import ${controller.name} from '../controllers/${controller.name}.controller'\n`
     });
     this.lineGenerating += `import { Routes } from '../interfaces/routes.interface';\n`;
     this.lineGenerating += `class ${routeName} implements Routes {\n`;
@@ -42,8 +43,11 @@ export class ExtensionService {
     extension.routes.forEach(route => {
       this.lineGenerating += `this.router.${route.type}(` + '`${this.path}`'
       route.controllers.forEach((controller) => {
-        this.lineGenerating += `,this.${controller.name}`
-        this.generateControllerService(controller.name);
+        this.lineGenerating += `,this.${controller.name}.test`
+        this.generateController(controller.name,route.service);
+      });
+      route.service.forEach((service)=>{
+        this.GenerateService(service);
       });
       this.lineGenerating += ');\n';
     });
@@ -52,12 +56,30 @@ export class ExtensionService {
     const saveExtension = this.electronService.ipcRenderer.sendSync('saveExtensionRouter', { path: pathFile, file: this.lineGenerating });
   }
 
-  generateControllerService(controllerName: string) {
+  generateController(controllerName: string,routeService:ServicesExtension[]) {
     let lineGenerating="import { NextFunction, Request, Response } from 'express';\n";
-    lineGenerating+=`class ${controllerName} {`;
-    lineGenerating+='}';
+    routeService.forEach((service)=>{
+      lineGenerating+=`import ${service.name} from '../services/${service.name}.service'\n`;
+    });
+    lineGenerating+=`class ${controllerName} {\n`;
+    routeService.forEach((service)=>{
+      lineGenerating+=`private ${service.name}=new ${service.name};\n`;
+    });
+    lineGenerating+='test(req:Request, rep:Response,next:NextFunction){}\n'
+    lineGenerating+='}\n';
     lineGenerating+=`export default ${controllerName};\n`;
     const file = this.configService.config.filePath + '/src/controllers/' + controllerName + '.controller.ts'
+    const exist = this.electronService.ipcRenderer.sendSync('ifFile', { path: file });
+    if (exist !== 'found') {
+      const saveExtensionController = this.electronService.ipcRenderer.sendSync('saveExtensionController', { path: file, file: lineGenerating });
+    }
+  }
+
+  GenerateService(service:ServicesExtension){
+    let lineGenerating=`class ${service.name} {\n`;
+    lineGenerating+='}\n';
+    lineGenerating+=`export default ${service.name};\n`;
+    const file = this.configService.config.filePath + '/src/services/' + service.name + '.service.ts'
     const exist = this.electronService.ipcRenderer.sendSync('ifFile', { path: file });
     if (exist !== 'found') {
       const saveExtensionController = this.electronService.ipcRenderer.sendSync('saveExtensionController', { path: file, file: lineGenerating });
